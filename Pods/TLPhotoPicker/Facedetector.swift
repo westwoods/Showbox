25d316d6eebd4eca89d3dcfe4752d3dd5d90c161
westwoods <kdw8681@gmail.com>
2017-08-04 11:23:11 +0900

    대공사전 커밋!

--- Pods/TLPhotoPicker/Facedetector.swift
+++ Pods/TLPhotoPicker/Facedetector.swift
@@ -3,7 +3,7 @@ open class FaceDetector{
     
 static  let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyLow]
 static  let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
-//faceDetector가 데이터를 저장해서 새로운 디텍터를 만들기보다는 같은것을 계속사용하는것을 추천함
+//faceDetector가 상태정보를 저장해서 새로운 디텍터를 만들기보다는 같은것을 계속사용하는것을 추천함
 class  func detect(uiImage: UIImage)  -> [CIFeature]? {
         let imageOptions = [CIDetectorSmile : true ,CIDetectorEyeBlink: true]
         let personciImage = CIImage(cgImage: uiImage.cgImage!)
