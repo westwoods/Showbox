cbf8fef91fc15b3264837e0d61f21468824bb039
westwoods <kdw8681@gmail.com>
2017-08-14 18:25:12 +0900

    타임라인 프리뷰 제작중
    시간에 따라 프리뷰셀의 크기를 바꾸는 작업이 필요함.

---
+++ ShowBox/PreViewCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PreViewCollectionViewCell.swift
+//  ShowBox
+//
+//  Created by snow on 2017. 8. 14..
+//  Copyright © 2017년 snow. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PreViewCollectionViewCell:UICollectionViewCell{
+
+	@IBOutlet var preViewImage: UIImageView!
+
+
+}
--- ShowBox/ShowBoxViewController.swift
+++ ShowBox/ShowBoxViewController.swift
@@ -10,7 +10,31 @@ import AVFoundation
 import UIKit
 
 
-class ShowBoxViewController: UIViewController {
+class ShowBoxViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource{
+	@IBOutlet var preViewCollectionView: UICollectionView!
+	fileprivate var searches:[UIImage] = []
+	//1
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return searches.count
+	}
+	
+	//2
+	func collectionView(_ collectionView: UICollectionView,
+	                    numberOfItemsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	//3
+ func collectionView(_ collectionView: UICollectionView,cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+	let cellIdentifier = "PreViewCollectionViewCell"
+	
+	guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? PreViewCollectionViewCell  else {
+		fatalError("The dequeued cell is not an instance of MusicTableViewCell.")
+	}
+	//print(indexPath.section)
+	cell.preViewImage.image = searches[indexPath.section]
+	return cell
+	}
 	
 	@IBOutlet var ShowBox: UIView!
 	
@@ -26,13 +50,12 @@ class ShowBoxViewController: UIViewController {
 	
 	func videoout(composition:AVComposition,mutableVideoCom:AVMutableVideoComposition,layer:CALayer){
 		DispatchQueue.main.async {
-		self.selectedAsset?.removeAll()
-        let playerItem = AVPlayerItem(asset: composition)
-        playerItem.videoComposition = mutableVideoCom//비디오 컴포지션 설정
-		let synclayer:AVSynchronizedLayer = AVSynchronizedLayer.init(playerItem: playerItem)
-		synclayer.addSublayer(layer )
-		
-		let player = AVPlayer(playerItem: playerItem)
+			let playerItem = AVPlayerItem(asset: composition)
+			playerItem.videoComposition = mutableVideoCom//비디오 컴포지션 설정
+			let synclayer:AVSynchronizedLayer = AVSynchronizedLayer.init(playerItem: playerItem)
+			synclayer.addSublayer(layer )
+			
+			let player = AVPlayer(playerItem: playerItem)
 			print (composition.duration)
 			let playerLayer = AVPlayerLayer(player: player)
 			playerLayer.frame = self.ShowBox.layer.bounds
@@ -45,6 +68,39 @@ class ShowBoxViewController: UIViewController {
 			playerLayer.addSublayer(synclayer)
 			self.ShowBox.layer.addSublayer(playerLayer)
 			player.play()
+			DispatchQueue.global().async {
+				
+			let imageGenerator = AVAssetImageGenerator(asset: composition)
+				imageGenerator.videoComposition = mutableVideoCom
+			var actualTime = kCMTimeZero
+			var thumbnail : CGImage?
+			print ("마지막 시간",Int((self.selectedAsset?.getTimes().last?.timePlayEnd.seconds)!))
+			for i in 0..<(self.selectedAsset?.getTimes().count)!{
+				if let tempAsset = self.selectedAsset?.getTimes()[i]{
+					if tempAsset.type == TimeAsset.AssetType.video{
+						//	비디오 타입
+						for timeScope in Int(tempAsset.timeStart.seconds)..<Int(tempAsset.timePlayEnd.seconds){
+							do {
+								thumbnail = try imageGenerator.copyCGImage(at: CMTimeMakeWithSeconds(Float64(timeScope)+0.5, 1000), actualTime: &actualTime)
+								self.searches.append(UIImage(cgImage: thumbnail!))
+							}
+							catch let error as NSError {
+								print(error.localizedDescription)
+							}
+						}
+					}
+					else {
+						//포토 타입
+						self.searches.append(tempAsset.passet!)
+					}
+				}
+			}
+				DispatchQueue.main.async {
+				self.preViewCollectionView.reloadData()
+				}
+				self.selectedAsset?.removeAll()
+			}
+			
 		}
 	}
 }
